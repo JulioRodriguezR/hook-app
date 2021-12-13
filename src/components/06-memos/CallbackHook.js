8b37abd590ee27753ca8b47dbb279640c80b5fc2
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2021-12-13 12:56:11 +0100

    implement useCallBack

--- src/components/06-memos/CallbackHook.js
+++ src/components/06-memos/CallbackHook.js
@@ -4,15 +4,19 @@ import '../02-useEffect/effects.css';
 import { ShowIncrement } from './ShowIncrement';
 
 export const CallbackHook = () => {
+  /**
+   * 1. Cuando mandamos una funcion a un componente hijo
+   * 2. Cuando existe un usEffect, para que no se dispare cada vez que se renderice 
+   */
 
   const [counter, setCounter] = useState(10);
 
-  const increment = () => {
-    setCounter(counter + 1);
-  }
+  // const increment = () => {
+  //   setCounter(counter + 1);
+  // }
 
-  useCallback(() => {
-    setCounter(c => c + 1);
+  const increment = useCallback((num) => {
+    setCounter(c => c + num);
   }, [setCounter]);
 
   return (
--- src/components/06-memos/ShowIncrement.js
+++ src/components/06-memos/ShowIncrement.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export const ShowIncrement = React.memo(({increment}) => {
-  console.log('Entra');
+  console.log('Increment');
   return (
     <div>
-     <button onClick={() => increment()} className="btn btn-primary">+1</button> 
+     <button onClick={() => increment(5)} className="btn btn-primary">+1</button> 
     </div>
   )
 });
