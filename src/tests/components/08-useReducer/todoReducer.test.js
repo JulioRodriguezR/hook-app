74cc40703afcb2d7edcd2602b795cc4fe83478b2
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2021-12-20 16:33:05 +0100

    More reducer tests

--- src/tests/components/08-useReducer/todoReducer.test.js
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -28,4 +28,35 @@ describe('Test in todoReducer', () => {
     expect(state).toEqual([...demoTodos, newTodo]);
   });
 
+  test('Should delete TODO', () => {
+    const todoToDelete = {
+      id: 3,
+      todo: 'Todo 3',
+      done: false
+    };
+    const action = {
+      type: 'delete',
+      payload: todoToDelete
+    }
+
+    const state = todoReducer(demoTodos, action);
+    console.log(state);
+
+    expect(state.length).toBe(2);
+  });
+
+  test('Should toggle TODO', () => {
+    const action = {
+      type: 'toggle',
+      payload: 1 
+    }
+
+    const state = todoReducer(demoTodos, action);
+    console.log(state);
+
+    expect(state[0].done).toBe(true);
+    expect(state[1]).toBe(demoTodos[1]);
+
+  });
+
 });
