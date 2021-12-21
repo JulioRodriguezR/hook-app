a49cdbb2f40a621f58b33fb9a24909ee0b339f10
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2021-12-21 16:53:22 +0100

    Add todolist test

--- src/components/08-useReducer/TodoList.js
+++ src/components/08-useReducer/TodoList.js
@@ -3,21 +3,21 @@ import React from 'react'
 import './styles.css';
 import { TodoListItem } from './TodoListItem';
 
-export const TodoList = ({ todos, handleDelete, handleToggle}) => {
+export const TodoList = ({ todos, handleDelete, handleToggle }) => {
 
   return (
-  <ul className="list-group list-group-flush">
-    {
-      todos.map((todo, i) =>
-      <TodoListItem 
-      key={todo.id}
-      todo={todo}
-      index={i}
-      handleDelete={handleDelete}
-      handleToggle={handleToggle}
-      />
-      )
-    }
+    <ul className="list-group list-group-flush">
+      {
+        todos.map((todo, i) => (
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            index={i}
+            handleDelete={handleDelete}
+            handleToggle={handleToggle}
+          />
+        ))
+      }
     </ul>
   );
 
--- src/tests/components/08-useReducer/TodoList.test.js
+++ src/tests/components/08-useReducer/TodoList.test.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { TodoList } from '../../../components/08-useReducer/TodoList';
+import { demoTodos } from '../fixtures/demoTodo';
 
 describe('Test in <TodoList />', () => {
-  
+
+  const handleDelete = jest.fn();
+  const handleToggle = jest.fn();
+
+  const wrapper = shallow(
+    <TodoList
+      todos={demoTodos}
+      handleDelete={handleDelete}
+      handleToggle={handleToggle}
+    />
+  );
+
   test('Should show correctly', () => {
-   const wrapper = shallow(<TodoList />);
-   expect(wrapper).toMatchSnapshot(); 
+    expect(wrapper).toMatchSnapshot();
   });
-  
+
+  test('Should 2 items TodoList', () => {
+    expect(wrapper.find('TodoListItem').length).toBe(demoTodos.length);
+  }); 
+
 });
